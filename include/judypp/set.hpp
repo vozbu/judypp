6a57748f8a16d420365b2690ab38183b89dc3f21
Valeriy Bykov <v.bykov@corp.mail.ru>
2012-05-31 15:20:05 +0400

    [+] License in header of each file

--- include/judypp/set.hpp
+++ include/judypp/set.hpp
@@ -1,3 +1,24 @@
+/*
+ * judypp - C++ bindings for the Judy library.
+ * Copyright (C) 2012 Valeriy Bykov <valery.bickov@gmail.com>
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Judy:   http://judy.sourceforge.net/
+ * judypp: https://github.com/vozbu/judypp
+ */
+
 #ifndef __JUDYPP_SET_HPP__
 #define __JUDYPP_SET_HPP__
 
